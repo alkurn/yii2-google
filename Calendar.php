da1e552819691fa337f644e78bc497cd661fec38
Ganesh Mankar <gmankar@gmail.com>
2018-06-04 23:28:14 +0530

    Update

---
+++ Calendar.php
@@ -0,0 +1,103 @@
+<?php
+
+
+namespace alkurn\google;
+
+use yii\widgets\InputWidget;
+use yii\helpers\Html;
+use yii\web\View;
+
+class Calendar extends InputWidget
+{
+
+    const API_URL = '//maps.googleapis.com/maps/api/js?';
+    public $libraries = 'places';
+    public $language = 'en-US';
+    public $sensor = true;
+    public $apiKey = 'AIzaSyC2oRAljHGZArBeQc5OXY0MI5BBoQproWY';
+    public $autocompleteOptions = [];
+
+    function getClient()
+    {
+        $client = new \Google_Client();
+        $client->setApplicationName('Google Calendar API PHP Quickstart');
+        $client->setScopes(\Google_Service_Calendar::CALENDAR_READONLY);
+        $client->setAuthConfig('client_secret.json');
+        $client->setAccessType('offline');
+
+        // Load previously authorized credentials from a file.
+        $credentialsPath = $this->expandHomeDirectory('credentials.json');
+        if (file_exists($credentialsPath)) {
+            $accessToken = json_decode(file_get_contents($credentialsPath), true);
+        } else {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+
+            // Store the credentials to disk.
+            if (!file_exists(dirname($credentialsPath))) {
+                mkdir(dirname($credentialsPath), 0700, true);
+            }
+            file_put_contents($credentialsPath, json_encode($accessToken));
+            printf("Credentials saved to %s\n", $credentialsPath);
+        }
+        $client->setAccessToken($accessToken);
+
+        // Refresh the token if it's expired.
+        if ($client->isAccessTokenExpired()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+        }
+        return $client;
+    }
+
+    /**
+     * Expands the home directory alias '~' to the full path.
+     * @param string $path the path to expand.
+     * @return string the expanded path.
+     */
+    function expandHomeDirectory($path)
+    {
+        $homeDirectory = getenv('HOME');
+        if (empty($homeDirectory)) {
+            $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
+        }
+        return str_replace('~', realpath($homeDirectory), $path);
+    }
+
+    public function run()
+    {
+
+// Get the API client and construct the service object.
+        $client = $this->getClient();
+        $service = new \Google_Service_Calendar($client);
+
+// Print the next 10 events on the user's calendar.
+        $calendarId = 'primary';
+        $optParams = array(
+            'maxResults' => 10,
+            'orderBy' => 'startTime',
+            'singleEvents' => true,
+            'timeMin' => date('c'),
+        );
+        $results = $service->events->listEvents($calendarId, $optParams);
+
+        if (empty($results->getItems())) {
+            print "No upcoming events found.\n";
+        } else {
+            print "Upcoming events:\n";
+            foreach ($results->getItems() as $event) {
+                $start = $event->start->dateTime;
+                if (empty($start)) {
+                    $start = $event->start->date;
+                }
+                printf("%s (%s)\n", $event->getSummary(), $start);
+            }
+        }
+    }
+}
