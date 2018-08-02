b63d0c4eb810bac1fb1946f912d33fa3f8c2c753
Ganesh <ganesh.alkurn@gmail.com>
2018-08-02 14:37:13 +0530

    Update

--- Calendar.php
+++ Calendar.php
@@ -183,10 +183,10 @@ class Calendar
         ];
 
         $event = new \Google_Service_Calendar_Event($event);
-        pr($event);
         $calendarId = 'primary';
         $event = $service->events->insert($calendarId, $event);
         printf('Event created: %s\n', $event->htmlLink);
+        exit;
     }
 
 
