73fe56d62ec06625f3ac6ef7ca5fa80afedbb0fb
Ganesh <ganesh.alkurn@gmail.com>
2018-08-06 15:05:10 +0530

    Update

--- Calendar.php
+++ Calendar.php
@@ -105,62 +105,13 @@ class Calendar
         }
     }
 
-    public function create($summary, $location, $description, $start, $end, $emailArray = [])
+    public function create($event)
     {
         $client = $this->getClient();
         $service = new \Google_Service_Calendar($client);
-
-        /*$event = array(
-            'summary' => 'Google I/O 2015',
-            'location' => '800 Howard St., San Francisco, CA 94103',
-            'description' => 'A chance to hear more about Google\'s developer products.',
-            'start' => array(
-                'dateTime' => '2015-05-28T09:00:00-07:00',
-                'timeZone' => 'America/Los_Angeles',
-            ),
-            'end' => array(
-                'dateTime' => '2015-05-28T17:00:00-07:00',
-                'timeZone' => 'America/Los_Angeles',
-            ),
-            'recurrence' => array(
-                'RRULE:FREQ=DAILY;COUNT=2'
-            ),
-            'attendees' => array(
-                array('email' => 'lpage@example.com'),
-                array('email' => 'sbrin@example.com'),
-            ),
-            'reminders' => array(
-                'useDefault' => FALSE,
-                'overrides' => array(
-                    array('method' => 'email', 'minutes' => 24 * 60),
-                    array('method' => 'popup', 'minutes' => 10),
-                ),
-            ),
-        )*/
-        //pr($service);
-
-        $event = [
-            'summary' => $summary,
-            'location' => $location,
-            'description' => $description,
-            'start' => $start,
-            'end' => $end,
-            'recurrence' => ['RRULE:FREQ=DAILY;COUNT=2'],
-            'attendees' => $emailArray,
-            'reminders' => [
-                'useDefault' => FALSE,
-                'overrides' => [
-                    ['method' => 'email', 'minutes' => 24 * 60],
-                    ['method' => 'popup', 'minutes' => 10],
-                ],
-            ],
-        ];
-
         $event = new \Google_Service_Calendar_Event($event);
         $calendarId = 'primary';
         $event = $service->events->insert($calendarId, $event);
-
-        //printf('Event created: %s\n', $event->htmlLink);
         return $event;
     }
 }
