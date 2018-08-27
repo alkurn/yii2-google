563534b6d9fad20ca5c9ba21ee9d94dc7d59a1e6
Ganesh <ganesh.alkurn@gmail.com>
2018-08-27 14:06:59 +0530

    Update

--- Calendar.php
+++ Calendar.php
@@ -49,9 +49,9 @@ class Calendar
         return $client;
     }
 
+
     public function oAuth($token)
     {
-
         $client = new \Google_Client();
         $client->setApplicationName('Google Calendar API PHP Quickstart');
         $client->setDeveloperKey($this->apiKey);
@@ -59,13 +59,13 @@ class Calendar
         $client->setAuthConfig($this->authConfig);
         $client->setAccessType('offline');
 
-        if (!empty($this->redirectUri)) {
+        if ( !empty($this->redirectUri) ) {
             $client->setRedirectUri($this->redirectUri);
         }
-
         $accessToken = $client->fetchAccessTokenWithAuthCode($token);
         \Yii::$app->session['accessToken'] = $accessToken;
     }
+
 
     public function setRedirectUri($url)
     {
@@ -90,77 +90,15 @@ class Calendar
 
         $results = $service->events->listEvents($calendarId, $optParams);
         $items = $results->getItems();
-
-        if (empty($items)) {
-            print "No upcoming events found.\n";
-        } else {
-            print "Upcoming events:\n";
-            foreach ($results->getItems() as $event) {
-                $start = $event->start->dateTime;
-                if (empty($start)) {
-                    $start = $event->start->date;
-                }
-                printf("%s (%s)\n", $event->getSummary(), $start);
-            }
-        }
+        return empty($items) ? false : $items;
     }
 
-    public function create($summary, $location, $description, $start, $end, $emailArray = [])
+    public function create($event)
     {
         $client = $this->getClient();
         $service = new \Google_Service_Calendar($client);
-
-        /*$event = array(
-            'summary' => 'Google I/O 2015',
-            'location' => '800 Howard St., San Francisco, CA 94103',
-            'description' => 'A chance to hear more about Google\'s developer products.',
-            'start' => array(
-                'dateTime' => '2015-05-28T09:00:00-07:00',
-                'timeZone' => 'America/Los_Angeles',
-            ),
-            'end' => array(
-                'dateTime' => '2015-05-28T17:00:00-07:00',
-                'timeZone' => 'America/Los_Angeles',
-            ),
-            'recurrence' => array(
-                'RRULE:FREQ=DAILY;COUNT=2'
-            ),
-            'attendees' => array(
-                array('email' => 'lpage@example.com'),
-                array('email' => 'sbrin@example.com'),
-            ),
-            'reminders' => array(
-                'useDefault' => FALSE,
-                'overrides' => array(
-                    array('method' => 'email', 'minutes' => 24 * 60),
-                    array('method' => 'popup', 'minutes' => 10),
-                ),
-            ),
-        )*/
-        //pr($service);
-
-        $event = [
-            'summary' => $summary,
-            'location' => $location,
-            'description' => $description,
-            'start' => $start,
-            'end' => $end,
-            'recurrence' => ['RRULE:FREQ=DAILY;COUNT=2'],
-            'attendees' => $emailArray,
-            'reminders' => [
-                'useDefault' => FALSE,
-                'overrides' => [
-                    ['method' => 'email', 'minutes' => 24 * 60],
-                    ['method' => 'popup', 'minutes' => 10],
-                ],
-            ],
-        ];
-
         $event = new \Google_Service_Calendar_Event($event);
-        $calendarId = 'primary';
-        $event = $service->events->insert($calendarId, $event);
-
-        //printf('Event created: %s\n', $event->htmlLink);
-        return $event;
+        $event = $service->events->insert('primary', $event);
+        return $event ? true : false;
     }
 }
